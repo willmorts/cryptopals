2f8aecfdef5978d9138eb5cf63b7f8bad3938ead
William Morton <willmorts@live.com>
2016-08-25 13:13:21 +0545

    Solutions and tests up to Set 1 Challenge 4

---
+++ Set 1/setonechallenges.py
@@ -0,0 +1,116 @@
+import sys
+import array
+import base64
+import json
+import re #used in Set 1, Challenge 4
+
+#================ Set 1, Challenge 1 ==================
+def hex_to_64(hex):
+    hex_data = hex.decode("hex")
+    result = base64.b64encode(hex_data)
+        
+    return result
+        
+#================ Set 1, Challenge 2 ================== 
+def fixed_xor(hex_one,hex_two): 
+    int_data_one = int(hex_one,16)
+    int_data_two = int(hex_two,16)
+        
+    return hex(int_data_one^int_data_two)[2:][:-1]
+
+#================ Set 1, Challenge 3 ================== 
+def single_byte_xor_cipher(hex):
+    hex_data = hex.decode("hex")
+    
+    num_xor_to_check = 128
+    success_each_xor = [None] * num_xor_to_check
+    
+    for cipher_test in range(0,num_xor_to_check):
+        byte_array = bytearray(hex_data)
+        ascii_array = []
+        string_result_arr = []
+        for index in range(0, len(byte_array)):
+            new_char = byte_array[index]^cipher_test
+            ascii_array.append(new_char)
+            
+        success_each_xor[cipher_test] = check_score_english_frequency(ascii_array)
+        string_result = stringify_ascii_array(ascii_array)
+
+#    Iterate over success_each_xor and return the top ranking one 
+    min_score = min(success_each_xor)
+    index_of_min = success_each_xor.index(min(success_each_xor))
+
+#    Re-xor the right string (hopefully!)
+    byte_array = bytearray(hex_data)
+    ascii_array = []
+    string_result_arr = []
+    for index in range(0, len(byte_array)):
+        new_char = byte_array[index]^index_of_min
+        ascii_array.append(new_char)
+        
+    string_result = stringify_ascii_array(ascii_array)
+
+    return string_result,min_score
+    
+#================ Set 1, Challenge 4 ================== 
+def detect_single_char_xor(file_to_test):
+    with open(file_to_test) as data_file:    
+        hex_data = data_file.readlines()
+        
+    array_of_scores_each = []
+    
+    for index in range(0,len(hex_data)): 
+        #Remove spaces from hex data
+        hex_data[index] = re.sub('[^0-9a-zA-Z]+', '', hex_data[index])
+        array_of_scores_each.append(single_byte_xor_cipher(hex_data[index])[1])
+        
+    min_score = min(array_of_scores_each)
+    index_of_min = array_of_scores_each.index(min_score)
+    
+    result = single_byte_xor_cipher(hex_data[index_of_min])[0] 
+    #Remove any new line instructions etc
+    result = re.sub('[^0-9a-zA-Z ]+', '', result)
+    
+    return result,min_score
+
+#================ Set 1, Challenges 3 & 4 useful function ================== 
+    
+# FUNCTION CHECKS HOW WELL IT MATCHES TO THE FREQUENCY OF CHARACTERS IN ENGLISH PLAINTEXT
+def check_score_english_frequency(ascii_array):
+    experiment_data = {'a': 0.000, 'b': 0.000, 'c': 0.000, 
+        'd': 0.000, 'e': 0.000, 'f': 0.000, 'g': 0.000, 'h': 0.000, 
+        'i': 0.000, 'j': 0.000, 'k': 0.000, 'l': 0.000, 'm': 0.000,
+        'n': 0.000, 'o': 0.000, 'p': 0.000, 'q': 0.000, 'r': 0.000,
+        's': 0.000, 't': 0.000, 'u': 0.000, 'v': 0.000, 'w': 0.000,
+        'x': 0.000, 'y': 0.000, 'z': 0.000}
+    total = 0
+    
+    with open('letterFrequencies.json') as data_file:    
+        data = json.load(data_file)
+    
+    for ascii_character in ascii_array:
+        character = (unichr(ascii_character))
+        if character in experiment_data:
+            experiment_data[character] += 1
+            total += 1
+    
+#    Change experiment data to percentages
+    if (total != 0):
+        for key in experiment_data:
+            experiment_data[key] = (experiment_data[key]/total)*100
+            
+#    Change experiment data to difference from actual percentages
+#    (simple just %actual - %experiment, modulus)
+    sum_differences = 0
+    for key in experiment_data: 
+        experiment_data[key] = abs(experiment_data[key] - data[key])
+        sum_differences += experiment_data[key]
+
+    return sum_differences 
+    
+def stringify_ascii_array(ascii_array): 
+    string_arr = []
+    for character in ascii_array:
+        string_arr.append(unichr(character))
+    
+    return ''.join(string_arr)    
---
+++ Set 1/setonetests.py
@@ -0,0 +1,24 @@
+import unittest
+from setonechallenges import *
+
+class TestMethods(unittest.TestCase):
+    
+#    Set 1, Challenge 1
+    def test_hex(self):
+        self.assertEqual(hex_to_64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'),
+        'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
+        
+#    Set 1, Challenge 2        
+    def test_xor(self):
+        self.assertEqual(fixed_xor('1c0111001f010100061a024b53535009181c','686974207468652062756c6c277320657965'),'746865206b696420646f6e277420706c6179') 
+          
+#    Set 1, Challenge 3
+    def test_decode_xor(self):
+        self.assertEqual(single_byte_xor_cipher('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')[0], "Cooking MC's like a pound of bacon")
+#    
+##    Set 1, Challenge 4
+    def test_detect_single_xor(self):
+        self.assertEqual(detect_single_char_xor("Set1Challenge4SuppliedFile.txt")[0], "Now that the party is jumping")
+   
+if __name__ == "__main__":
+    unittest.main()
